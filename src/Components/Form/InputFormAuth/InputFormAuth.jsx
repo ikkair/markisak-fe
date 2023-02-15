b64dc2ef36272cd9963076c51f553d32c90d2f67
Dhimas Pandu Yogaswara <dhimaspanduyoga@gmail.com>
2023-02-15 20:46:44 +0700

    Add fiture Register

--- src/Components/Form/InputFormAuth/InputFormAuth.jsx
+++ src/Components/Form/InputFormAuth/InputFormAuth.jsx
@@ -1,11 +1,12 @@
 import React, { Children } from 'react'
+import style from '../../Layout/AuthLayout/style.module.css'
 
 const InputFormAuth = (props) => {
     return (
         <>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">{props.title}</label>
-                <input type={props.type} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name={props.name}/>
+                <label for="exampleInputEmail1" class={`form-label ${style.formLabel}`}>{props.title}</label>
+                <input type={props.type} class={`form-control ${style.formControl}`} id="exampleInputEmail1" aria-describedby="emailHelp" name={props.name} placeholder={props.title}/>
             </div>
         </>
     )
