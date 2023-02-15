8005bf523bdbfe14f7227cb0465f4b322d9086e8
rizalyuniar <rizalyuniar6@gmail.com>
2023-02-16 01:00:43 +0700

    commit profil footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import CreateRecipe from './Pages/Dashboard/CreateRecipe/CreateRecipe';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import Profile from './Pages/Profile/Profile';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/dashboard/create-recipe" element={<CreateRecipe />} />
       <Route path="/login" element={<Login/>} />
+      <Route path="/profile" element={<Profile/>} />
 
     </Routes>
   );
