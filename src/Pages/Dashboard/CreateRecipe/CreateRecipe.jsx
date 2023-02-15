9cea6667f2c031de255011aada6676a2fbb6e812
mhmdiamd <darmawanilham34@gmail.com>
2023-02-15 18:02:30 +0700

    Page Create Product with responsive

--- src/Pages/Dashboard/CreateRecipe/CreateRecipe.jsx
+++ src/Pages/Dashboard/CreateRecipe/CreateRecipe.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
-import { useCreateRecipeMutation, useGetAllRecipeQuery } from '../../../Features/recipe/recipeApi';
+import React, { useEffect, useState } from 'react';
+import { useCreateRecipeMutation, useDeleteRecipeByIdMutation, useGetAllRecipeQuery } from '../../../Features/recipe/recipeApi';
+import Navbar from './../../../Components/Navbar/Navbar';
+import photoLogo from '../../../assets/createRecipe/image.png';
+import style from './style.module.css';
+import InputFormAddRecipe from './../../../Components/Form/InputFormAddRecipe/InputFormAddRecipe';
 
 const CreateRecipe = () => {
   const { data: dataAllRecipe } = useGetAllRecipeQuery();
   const [createRecipe, { isLoading, error }] = useCreateRecipeMutation();
+  const [deleteRecipe, { error: errorDeleteRecipe, isSuccess }] = useDeleteRecipeByIdMutation();
+  const [selectedFile, setSelectedFile] = useState();
+  const [preview, setPreview] = useState();
   const [data, setData] = useState({
     title: '',
     photo: '',
@@ -11,8 +18,7 @@ const CreateRecipe = () => {
     video: '',
   });
 
-  const onChangeHandler = (e) => {
-    console.log(data);
+  const changeHandler = (e) => {
     setData((prev) => {
       return {
         ...prev,
@@ -21,36 +27,78 @@ const CreateRecipe = () => {
     });
   };
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    await createRecipe(data);
+  const createHandler = async () => {
+    await createRecipe({ ...data, photo: preview });
   };
 
+  function imageClickHandler(e) {
+    const inputImg = document.querySelector(`#photo`);
+    inputImg.click();
+  }
+
+  const selectFile = (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setSelectedFile(undefined);
+      return;
+    }
+
+    setSelectedFile(e.target.files[0]);
+  };
+
+  useEffect(() => {
+    if (!selectedFile) {
+      setSelectedFile(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(selectedFile);
+    setPreview(objectUrl);
+
+    // free memory when ever this component is unmounted
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [selectedFile]);
+
   return (
-    <div>
-      <ul>
-        {dataAllRecipe?.map((recipe) => (
-          <li>
-            {recipe.title} <button className="btn btn-danger">Delete</button>{' '}
-          </li>
-        ))}
-      </ul>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="title">Title</label>
-        <input type="text" name="title" onChange={onChangeHandler} />
+    <>
+      <Navbar />
+      <div className="container pb-5">
+        <div className="row">
+          <div className="col-12 ">
+            <div className="row">
+              <div className="col-12 col-md-10 offset-md-1 d-flex justify-content-center">
+                <div className={`item w-100 rounded ${style.inputBackground} ${style.inputPhoto} d-flex mx-auto justify-content-center align-items-center mb-2 flex-column mt-5`} onClick={imageClickHandler} id="thumbnail">
+                  <img src={preview ? preview : photoLogo} alt="" className="img-fluid" />
+                  <span className="text-secondary mt-2">Add Photo</span>
 
-        <label htmlFor="title">Photo</label>
-        <input type="text" name="photo" onChange={onChangeHandler} />
+                  <input type="file" className="d-none" name="photo1" onChange={selectFile} id={`photo`} />
+                </div>
+              </div>
 
-        <label htmlFor="title">Ingredients</label>
-        <input type="text" name="ingredients" onChange={onChangeHandler} />
+              <div className="col-12 col-md-10 offset-md-1 mt-4">
+                <InputFormAddRecipe type={'text'} title={'Title'} name={'title'} onchange={(e) => changeHandler(e)} />
+              </div>
 
-        <label htmlFor="title">Video</label>
-        <input type="text" name="video" onChange={onChangeHandler} />
+              <div className="col-12 col-md-10 offset-md-1 mt-4">
+                <InputFormAddRecipe type={'textarea'} title={'Ingredients'} name={'ingredients'} onchange={(e) => changeHandler(e)} />
+              </div>
 
-        <input type="submit" value={'kirim'} />
-      </form>
-    </div>
+              <div className="col-12 col-md-10 offset-md-1 mt-4">
+                <div className={`item rounded ${style.inputBackground} mx-auto d-flex p-3 px-5 gap-3`} id="thumbnail">
+                  <span className="text-secondary mt-2 text-nowrap text-dark fw-semibold">Video</span>
+                  <input type="text" className="form-control bg-transparent border-0 border-bottom rounded-0 outline-none" name="video" onChange={changeHandler} />
+                </div>
+              </div>
+
+              <div className="col-12 col-md-10 offset-md-1 mt-5 d-flex justify-content-center">
+                <button className="btn btn-warning w-50 text-light" onClick={createHandler}>
+                  Post
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
