f1733c175d3e52957aac6eb96328e58252631367
mhmdiamd <darmawanilham34@gmail.com>
2023-02-16 22:40:56 +0700

    Create Popular Recipe in Landing page using Swiper

--- src/Components/Section/LandingPageSection/LandingPageSection.jsx
+++ src/Components/Section/LandingPageSection/LandingPageSection.jsx
@@ -3,10 +3,10 @@ import style from './style.module.css';
 
 const LandingPageSection = ({ title, embedClass, children }) => {
   return (
-    <div className={`${embedClass} ${style.sectionContainer} pt-5 container`}>
+    <div className={`${embedClass} ${style.sectionContainer} container`}>
       <div className="row">
-        <div className="col-12 py-3">
-          <span className={`${style.borderTitle} fs-4 ps-2 py-3 fw-semibold d-block mb-4`}>{title}</span>
+        <div className={`${style.columnTitle} col-12 py-3`}>
+          <span className={`${style.borderTitle} ${style.title} fs-2 ps-2 py-2 fw-semibold d-block`}>{title}</span>
         </div>
         {children}
       </div>
