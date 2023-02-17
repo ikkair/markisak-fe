c4d4cb6e543cb325a31a8192df7505b1500c79cb
mhmdiamd <darmawanilham34@gmail.com>
2023-02-17 16:49:26 +0700

    fix card recipe profile

--- src/Components/Profile/Card.jsx
+++ src/Components/Profile/Card.jsx
@@ -1,39 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import img from '../../assets/Profile/img1.png'
-import img2 from "../../assets/Profile/img2.png";
-import style from './style.module.css'
 
-let Cards = [
-    {
-        name: "Bomb Chicken",
-        photo: `${img}`
-    },
-    {
-        name: "Bananas Pancake",
-        photo: `${img2}`
-    }
-];
+import style from './style.module.css';
 
-const Card = () => {
-    return (
-        <div>
-            <div className='container mb-5'>
-                <div className='d-flex'>
-                    {Cards.map((item) => (
-                        <>
-                            <Link class={`mx-2 ${style.card}`} to="#">
-                                <img src={item.photo} class="card-img-top" alt="..." />
-                                <div class={`card-body ${style.cardBody}`}>
-                                    <h5 class="card-title">{item.name}</h5>
-                                </div>
-                            </Link>
-                        </>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
-}
+const Card = ({ item }) => {
+  return (
+    <Link to={`/recipes/${item.id}`} class={` ${style.card}`}>
+      <img src={item?.photo} class="card-img-top" alt="..." />
+      <div class={`card-body ${style.cardBody}`}>
+        <h5 class="card-title">{item?.name}</h5>
+      </div>
+    </Link>
+  );
+};
 
-export default Card
+export default Card;
