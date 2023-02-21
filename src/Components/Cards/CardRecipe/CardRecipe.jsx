0d8209dae64169ed2cfd97d9c3ff68b12cc0b607
mhmdiamd <darmawanilham34@gmail.com>
2023-02-22 03:53:33 +0700

    Git fix crossOrigin"

--- src/Components/Cards/CardRecipe/CardRecipe.jsx
+++ src/Components/Cards/CardRecipe/CardRecipe.jsx
@@ -6,9 +6,9 @@ import style from './style.module.css';
 const CardRecipe = ({ item }) => {
   return (
     <Link to={`/recipes/${item.id}`} className={`${style.card}`}>
-      <img src={item.photo} crossOrigin={'anonymous'} className={`img-fluid border border-2 border-light  ${style.img}`} alt="..." />
+      <img src={item.photo} className={`img-fluid border border-2 border-light  ${style.img}`} alt="..." />
       <div className={`${style.cardBody} w-100`}>
-        <h5 className={`${style.cardTitle} px-2`}>{item?.title} Lorem ipsum dolor sit amet.</h5>
+        <h5 className={`${style.cardTitle} px-2`}>{item?.title} </h5>
       </div>
     </Link>
   );
